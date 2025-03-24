7de12c98f1dc6bc55b8c1ed56a82a046bbac1cf1
jonas-bechtel <jonas.bechtel@t-online.de>
2025-03-24 14:30:48 +0100

    added premake file

---
+++ premkae5.lua
@@ -0,0 +1,32 @@
+project "implot"
+    kind "StaticLib"
+    language "C++"
+    staticruntime "on"
+
+    targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files 
+    {
+        "implot.h",
+        "implot_internal.h"
+        "implot.cpp"
+        "implot_demo.cpp"
+        "implot_items.cpp"
+    }
+
+    includedirs 
+    {
+        "./../imgui" -- if you also added ImGui as a sibling project
+    }
+
+    filter "system:windows"
+        systemversion "latest"
+
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
